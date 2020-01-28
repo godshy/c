7642444c724c0f32b4e8bd13e44e02580e57543e
godshy <asse9653@hotmail.com>
2020-01-29 00:47:04 +0800

    1

--- 01.c
+++ 01.c
@@ -2,6 +2,7 @@
 
 int main()
 {
-    printf('hello fucking world');
+    printf("hello fucking world!!");
+
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
-
-int main()
+int main ()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    
-
-      for (const string& word : msg)
-   {
-      cout << word << " ";
-   }
-   cout << endl;
+    using namespace std;
+    cout << "hello world\n";
+    return 0;
 }
